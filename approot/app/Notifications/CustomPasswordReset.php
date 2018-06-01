638ad19665d99a722a9d5892a0dd151ab4c609ed
osamuya <yamakami@3kaku.co.jp>
2018-06-01 17:23:03 +0900

    パスワードリセットカスタマイズ（途中）

---
+++ approot/app/Notifications/CustomPasswordReset.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+/* password resset */
+use Illuminate\Auth\Notifications\ResetPassword;
+
+class CustomPasswordReset extends Notification
+{
+    use Queueable;
+
+    /** @var string */
+    public $token;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    // ->line('The introduction to the notification.')
+                    // ->action('Notification Action', url('/'))
+                    // ->line('Thank you for using our application!');
+                    ->subject(__('Reset Password'))
+                    ->view('mails.reset')
+                    ->action(__('Reset Password'), url('password/reset', $this->token));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+
+
+}
