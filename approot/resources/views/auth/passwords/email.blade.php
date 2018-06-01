30cb87c0c6f4f36fc623908319116c150f5fa223
osamuya <yamakami@3kaku.co.jp>
2018-06-01 14:43:21 +0900

    パスワードリセット画面の日本語化

--- approot/resources/views/auth/passwords/email.blade.php
+++ approot/resources/views/auth/passwords/email.blade.php
@@ -5,12 +5,12 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="card-header">パスワード再設定</div>
 
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success">
-                            {{ session('status') }}
+                            パスワード再設定リンクを送信しました。<!--{{ session('status') }}-->
                         </div>
                     @endif
 
@@ -18,7 +18,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">Eメールアドレス</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
@@ -34,7 +34,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                                    パスワードをリセットする
                                 </button>
                             </div>
                         </div>
