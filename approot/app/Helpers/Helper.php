d73ae364bb5f36861a3ce5abd2ce16256bc8b718
osamuya <yamakami@3kaku.co.jp>
2018-06-04 21:52:59 +0900

    composer script for making log/app log files

--- approot/app/Helpers/Helper.php
+++ approot/app/Helpers/Helper.php
@@ -4,6 +4,12 @@ namespace App\Helpers;
 
 /* Datetime package "Carbon" for laravel */
 use Carbon\Carbon;
+/* For custom logger */
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Illuminate\Support\Facades\Log;
+
+
 
 class Helper
 {
@@ -13,16 +19,17 @@ class Helper
      * @param string $value
      * @return string
      */
-    public static function xxx($value)
+    public static function helloHelper()
     {
-        // 処理
-        return $value;
+      return "Hello helper!";
     }
 
     /**
      * ユニークIDを作成
-     *
-     *
+     * (例）PREFIX602-5b1510747bfeb
+     * これは本当にユニバーサルユニークなのか疑問
+     * @param string $prefix
+     * @return string
      *
      */
     public static function makeUniqueId($prefix="")
@@ -32,9 +39,11 @@ class Helper
     }
 
     /**
-     * ユニークIDを作成
-     *
-     *
+     * ユニークハッシュを作成
+     * (例）9a27488d86d8bf40c4661fc22356329be7fa15c55b300d916dc1db7efe9e24ea
+     * これはかなりユニークになる筈
+     * @param none
+     * @return string
      *
      */
      public static function makeUniqueHash()
@@ -46,7 +55,6 @@ class Helper
          env("APP_KEY"),
          $add_string
        );
-
        return $uniqehash;
      }
 
